c954d0af9219a5c3e5eb02d0e73b169067f0f6ff
Shadow316 <david.51699@gmail.com>
2024-12-07 21:43:46 -0600

    Modificaciones a config.py

---
+++ Folder orm./config.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+URL_BASE_DATOS = "postgresql://usuario_ejemplo:Aa3158134979@localhost:5432/bd_alumnos"
+
+engine = create_engine(URL_BASE_DATOS,
+                       connect_args={
+                           "options": "-csearch_path=app"  #Para que la BD se conecte por medio del esquema
+                       })
+
+SessionClass = sessionmaker(engine) 
+
+def generador_sesion():
+    sesion = SessionClass()
+    try:
+        #equivalente a return sesion pero de manera segura
+        yield sesion 
+    finally:
+        sesion.close()
+
+BaseClass = declarative_base()
