64c603bf2bde51b5d2180e8af2023245b740a44f
Shadow316 <david.51699@gmail.com>
2024-12-07 21:44:57 -0600

    cambios

---
+++ api.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, UploadFile, File, Form, Depends
+from typing import Optional
+from pydantic import BaseModel
+import shutil
+import os
+import uuid
+import orm.repo as repo # Función para hacer las consultas a la BD
+from sqlalchemy.orm import Session
+from sqlalchemy import and_
+from orm.config import generador_sesion # Generador de sesiones
+
+# Se crea el servidor
+app = FastAPI()
+
